31217c3dc671c0a25390e91593b693883aae8156
Andrew Bryant <ajbryant.01@gmail.com>
2011-10-23 20:20:17 +1000

    added sessions and signin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 SampleApp::Application.routes.draw do
  
 
+
+  resources :sessions, :only => [:new, :create, :destroy]
   resources :users
   
   root :to => "pages#home"
@@ -8,6 +10,8 @@ SampleApp::Application.routes.draw do
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/signup', :to => 'users#new'
+  match '/signin', :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
   #get "pages/home"
   #get "pages/about"
   #get "pages/contact"
