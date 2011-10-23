6c1144da22ffa09ac6e840f24a01225818f172fe
Andrew Bryant <ajbryant.01@gmail.com>
2011-10-24 01:59:07 +1000

    correct user requirement and forwarding on session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
  #If users are not signed in. Must limit access using before_filter
  #also look in the private methods for complete actions
  before_filter :authenticate, :only => [:edit, :update]
- 
+ before_filter :correct_user, :only => [:edit, :update]
  
  
   def new
@@ -49,5 +49,9 @@ class UsersController < ApplicationController
     deny_access unless signed_in?
   end
 
+  def correct_user
+   @user  = User.find(params[:id])
+   redirect_to(root_path) unless current_user?(@user)
+  end
   
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,42 +1,61 @@
 module SessionsHelper
   
   def sign_in(user)
-    #20 year salted cookie...pretzel?
-    #stops cookie transfer with salt encryption
     cookies.permanent.signed[:remember_token] = [user.id, user.salt]
-    current_user = user
+    self.current_user = user
   end
-  #setter
+  
   def current_user=(user)
     @current_user = user
   end
-  #getter || = or equals 
+  
   def current_user
     @current_user ||= user_from_remember_token
   end
   
-  #is user signed in? dbl neg
   def signed_in?
     !current_user.nil?
   end
   
-  #sign out method
   def sign_out
     cookies.delete(:remember_token)
-    current_user = nil
+    self.current_user = nil
   end
-    
+
+  def current_user?(user)
+    user == current_user
+  end
+  
+  def authenticate
+    deny_access unless signed_in?
+  end
+
   def deny_access
-    redirect_to signin_path, :notice => "Sign in to get access."
+    store_location
+    redirect_to signin_path, :notice => "Please sign in to access this page."
+  end
+  
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    clear_return_to
   end
   
   private
-  
-  def user_from_remember_token
-    User.authenticate_with_salt(*remember_token)
-  end
-  
-  def remember_token
-    cookies.signed[:remember_token] || [nil, nil]
-  end
+
+    def user_from_remember_token
+      User.authenticate_with_salt(*remember_token)
+    end
+    
+    def remember_token
+      cookies.signed[:remember_token] || [nil, nil]
+    end
+
+    def store_location
+      session[:return_to] = request.fullpath
+    end
+
+    def clear_return_to
+      session[:return_to] = nil
+    end
 end
+
